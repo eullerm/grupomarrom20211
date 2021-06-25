7174ddda4a088dd6b0d54575359064018b940f60
MalkaiOliveira <62706524+MalkaiOliveira@users.noreply.github.com>
2021-06-25 15:03:13 -0300

    Update do Auto_Route
    Fix 01

--- geocard/lib/AutoRoute/AutoRoute.dart
+++ geocard/lib/AutoRoute/AutoRoute.dart
@@ -1,10 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:geocard/landing.dart';
+import 'package:geocard/credits.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
     AutoRoute(page: Landing, initial: true),
+    AutoRoute(page: Credits),
   ],
 )
 class $AppRouter {}
