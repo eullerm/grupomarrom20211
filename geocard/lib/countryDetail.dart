dfafeb205a04cfcdbe5ab1721d970931dd842056
Euller Macena <49864491+eullerm@users.noreply.github.com>
2021-06-29 00:56:58 -0300

    Mudanças na página de detalhes do pais
    * Realizado a animação do scroll da tela onde será exibido a informação dos detalhes de cada páis.
    * Criado exemplo de como deve ser exibiado cada info.

--- geocard/lib/countryDetail.dart
+++ geocard/lib/countryDetail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geocard/Theme.dart';
 import 'package:geocard/widgets/background.dart';
-import 'package:geocard/widgets/button.dart';
 import 'package:geocard/widgets/cardInfo.dart';
 import 'package:geocard/widgets/title.dart';
 
@@ -13,6 +12,44 @@ class CountryDetail extends StatefulWidget {
 }
 
 class _CountryDetailState extends State<CountryDetail> {
+  ScrollController scrollControllerOverview = ScrollController();
+  ScrollController scrollControllerCardInfo = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollControllerOverview.addListener(() {
+      setState(() {
+        if (scrollControllerOverview.offset >=
+            scrollControllerOverview.position.maxScrollExtent / 2) {
+          scrollControllerCardInfo.animateTo(
+            scrollControllerCardInfo.position.maxScrollExtent,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.ease,
+          );
+        }
+        if (scrollControllerOverview.offset ==
+            scrollControllerOverview.position.minScrollExtent) {
+          scrollControllerCardInfo.animateTo(
+            scrollControllerCardInfo.position.minScrollExtent,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.ease,
+          );
+        }
+      });
+    });
+    scrollControllerCardInfo.addListener(() {
+      setState(() {
+        if (scrollControllerCardInfo.offset ==
+            scrollControllerCardInfo.position.minScrollExtent) {
+          scrollControllerOverview.jumpTo(
+            scrollControllerOverview.position.minScrollExtent,
+          );
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,25 +77,29 @@ class _CountryDetailState extends State<CountryDetail> {
                 // Detalhes
                 Flexible(
                   child: SingleChildScrollView(
+                    controller: scrollControllerCardInfo,
                     child: Container(
                       height: MediaQuery.of(context).size.height + 70,
                       child: Column(
                         children: <Widget>[
-                          SizedBox(height: 150),
+                          AnimatedContainer(
+                              duration: Duration(milliseconds: 200),
+                              height: 150),
                           CardInfo(isDetailPage: true),
                           SizedBox(height: 8),
-                          /*Divider(
-                            color: AppColorScheme.appText,
-                            height: 0,
-                            thickness: 2,
-                          ),*/
                           Flexible(
                             child: SingleChildScrollView(
+                              controller: scrollControllerOverview,
                               child: Column(
                                 children: <Widget>[
                                   SizedBox(height: 20),
-                                  Text(
-                                      "datadatadatadatadata\n\n\n\n\n\n\ndatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadata"),
+                                  //Exemplo de como cada campo de informação deve ser construido.
+                                  Row(
+                                    children: [
+                                      Text("Info: "),
+                                      Text("Informação sobre o campo info"),
+                                    ],
+                                  )
                                 ],
                               ),
                             ),
