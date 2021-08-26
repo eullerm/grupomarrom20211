52ccc69e02cce4d68777a4d0c2046785e6edb418
Hiaggo Bezerra <42723368+Hiaggo@users.noreply.github.com>
2021-08-26 11:58:07 -0300

    Mais questões adicionadas

--- grupomarrom20211/lib/const/questions.dart
+++ grupomarrom20211/lib/const/questions.dart
@@ -55,6 +55,83 @@ class Questions {
         "Nigéria": false,
       },
     },
+    {
+      "id": 5,
+      "question": "Qual país possui o menor produto interno nominal?",
+      "cards": {
+        "México": false,
+        "Egito": false,
+        "China": false,
+        "Índia": false,
+        "Angola": true,
+      },
+    },
+    {
+      "id": 6,
+      "question": "Dentre os seguintes países, qual é considerado o mais antigo?",
+      "cards": {
+        "Brasil": false,
+        "França": false,
+        "China": true,
+        "Índia": false,
+        "México": false,
+      },
+    },
+    {
+      "id": 7,
+      "question": "Quais dos seguintes países fizeram parte dos \"Aliados\" na Segunda Guerra Mundial?",
+      "cards": {
+        "Japão": false,
+        "França": true,
+        "China": true,
+        "EUA": true,
+        "Alemanha": false,
+      },
+    },
+    {
+      "id": 8,
+      "question": "Qual país possui a torre treliça mais famosa do mundo?",
+      "cards": {
+        "Japão": false,
+        "França": true,
+        "Brasil": false,
+        "Nigéria": false,
+        "Rússia": false,
+      },
+    },
+    {
+      "id": 9,
+      "question": "Quais países possuem alguma das \"Novas Maravilhas do Mundo\"?",
+      "cards": {
+        "Brasil": true,
+        "México": true,
+        "Índia": true,
+        "EUA": false,
+        "Alemanha": false,
+      },
+    },
+    {
+      "id": 10,
+      "question": "Quais países não fazem parte do G20?",
+      "cards": {
+        "Japão": false,
+        "França": false,
+        "Brasil": false,
+        "México": false,
+        "Angola": true,
+      },
+    },
+    {
+      "id": 11,
+      "question": "De onde origina o \"Kuduro\"?",
+      "cards": {
+        "Egito": false,
+        "Índia": false,
+        "México": false,
+        "Brasil": false,
+        "Angola": true,
+      },
+    },
   ];
   List get questions {
     return _questions;
